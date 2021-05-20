4b0c78234c52ef3d0935c7149f3206f96702dc0d
CJParry <chris.parry@windowslive.com>
2021-05-20 23:17:59 +1200

    bsc test token deployed

--- election/migrations/2_deploy_contracts.js
+++ election/migrations/2_deploy_contracts.js
@@ -5,11 +5,13 @@
 // };
 
 
-var BestToking = artifacts.require("./BestToking.sol");
+var BEP20Token = artifacts.require("./BEP20Token.sol");
 //var BestToking = artifacts.require("./BestToking.sol");
 
 module.exports = function(deployer) {
     //deployer.deploy(BestToking);
-    deployer.deploy(BestToking, 100, "Best Toking Token", 1, "TOKE");
+    //deployer.deploy(BestToking, 100, "Best Toking Token", 1, "TOKE");
+    deployer.deploy(BEP20Token, 100, "BSC Toking Token", 1, "BSCT");
+
 
 };
